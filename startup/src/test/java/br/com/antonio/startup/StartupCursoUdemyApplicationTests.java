834e96052444406ae1e671c31dc9fb9b9c5410b1
AntonioALino <antonioaplino@gmail.com>
2023-12-20 10:02:13 -0300

    Finalizando a sessão 5 do curso

---
+++ startup/src/test/java/br/com/antonio/startup/StartupCursoUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.antonio.startup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupCursoUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
